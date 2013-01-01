92a003397db3099ef082d7138e606f1337ddbe37
Billy Coover <billycoover@gmail.com>
2013-01-01 09:51:48 -0700

    added register event

--- ln.lua
+++ ln.lua
@@ -16,4 +16,22 @@ local log_event = function (PROJECT_CODE, PROJECT_SECRET, name, email, event)
     })
 end
 
-return { log_event = log_event }
+local register_event = function (PROJECT_CODE, PROJECT_SECRET, name, email, event, id)
+    local event = {
+        ['person[name]'] = name,
+        ['person[email]'] = email,
+        ['person[external_identifier]'] = id,
+        ['person[properties][created_at]'] = os.time()
+        ['event[name]'] = event,
+        ['event[klass]'] = 'actionevent'
+    }
+
+    return  http.request({
+        method = "post",
+        url = string.format(HOST, 'events'),
+        auth = {PROJECT_CODE, PROJECT_SECRET},
+        data = event
+    })
+end
+
+return { log_event = log_event, register_event = register_event }
